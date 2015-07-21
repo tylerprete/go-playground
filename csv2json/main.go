14e1219eb26c63a91191f56ddcc2428615d76717
Tyler Prete <psyonic@gmail.com>
2015-07-21 03:45:06 -0700

    Code cleanup.

--- csv2json/main.go
+++ csv2json/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
 	"encoding/csv"
-	"os"
+	"encoding/json"
+	"fmt"
 	"io"
-//	"strconv"
-	"bufio"
+	"os"
+	//	"strconv"
 )
 
 func zipMap(keys []string, values []string) map[string]string {
@@ -21,19 +20,19 @@ func zipMap(keys []string, values []string) map[string]string {
 func converter(keys []string, line []string) string {
 	val, err := json.Marshal(zipMap(keys, line))
 	if err != nil {
-		fmt.Println("ERROR IN CONVERTER!",err)
+		fmt.Println("ERROR IN CONVERTER!", err)
 	}
 	return string(val)
 }
 
 func convertWorker(worker int, keys []string, c <-chan []string, done chan<- bool) {
-//	fmt.Println("Worker",worker,"created!")
+	//	fmt.Println("Worker",worker,"created!")
 	for {
-		s, more := <- c
-		if more == true {
+		s, more := <-c
+		if more {
 			fmt.Println(converter(keys, s))
 		} else {
-//			fmt.Println("Worker",worker,"ending!")
+			//			fmt.Println("Worker",worker,"ending!")
 			break
 		}
 	}
@@ -43,10 +42,10 @@ func convertWorker(worker int, keys []string, c <-chan []string, done chan<- boo
 func main() {
 	workers := 4
 	rows := make(chan []string, workers)
-	worker_done := make(chan bool, workers)
+	worker_done := make(chan bool)
+	defer close(worker_done)
 
-	in := bufio.NewReader(os.Stdin)
-	reader := csv.NewReader(in)
+	reader := csv.NewReader(os.Stdin)
 	reader.Comma = ','
 
 	// Read first line.
@@ -55,7 +54,7 @@ func main() {
 		fmt.Println("Must have input from STDIN.")
 		return
 	} else if err != nil {
-		fmt.Println("Unexpected error reading header:",err)
+		fmt.Println("Unexpected error reading header:", err)
 	}
 	//	fmt.Println("Header: ",first)
 
@@ -66,7 +65,6 @@ func main() {
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
-	//			fmt.Println("EOF")
 			break
 		} else if err != nil {
 			fmt.Println("Unexpected error reading line", err)
@@ -75,9 +73,7 @@ func main() {
 		rows <- record
 	}
 	close(rows)
-	rows_done := workers
-	for rows_done > 0 {
-		<- worker_done
-		rows_done -= 1
+	for rows_done := workers; rows_done > 0; rows_done-- {
+		<-worker_done
 	}
 }
